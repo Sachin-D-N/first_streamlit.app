4f4948aa61e27c547007e3e04cbba8d53cfa8738
SACHIN D N <67965686+Sachin-D-N@users.noreply.github.com>
2023-06-28 02:22:03 +0530

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,21 +30,25 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #user chice
+def get_fruity_wise_data(fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+  #streamlit.text(fruityvice_response.json())
+  #write your own comment -normalize the output
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 try :
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('please select a fruit to get information')
   else:
-      #streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
-      #streamlit.text(fruityvice_response.json())
-      #write your own comment -normalize the output
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # write your own comment - converts in to dataframe
-      streamlit.dataframe(fruityvice_normalized)
-    
-except URLError as e:
+    #streamlit.write('The user entered ', fruit_choice)
+    back_from_function=get_fruity_wise_data(fruit_choice)
+      
+    # write your own comment - converts in to dataframe
+    streamlit.dataframe(fruit_choice)
+ except URLError as e:
     streamlit.error()
+
 
 
 
